6d3375ea5518741efd35e4e806c42279b97a9960
052-PRANAVMV <pranav19072005@gmail.com>
2025-02-07 13:41:49 +0530

    Build fix

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,50 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path'); 
-const upload = require('./config/multerConfig'); // Import the Multer configuration
+    const express = require('express');
+    const dotenv = require('dotenv');
+    const mongoose = require('mongoose');
+    const cookieParser = require('cookie-parser');
+    const bodyParser = require('body-parser');
+    const cors = require('cors');
+    const path = require('path'); 
+    const upload = require('./config/multerConfig'); // Import the Multer configuration
+    const adminRoutes = require('./routes/Admin');
+    const FacultyRoutes = require('./routes/Faculty');
 
-dotenv.config();
+    dotenv.config();
 
-const adminRoutes = require('./routes/Admin');
-const FacultyRoutes = require('./routes/Faculty');
+    // Database connection
+    mongoose.connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then((con) => {
+        console.log(`MongoDB Connected to host: ` + con.connection.host);
+    }).catch(err => {
+        console.error('MongoDB connection error:', err.message);
+    });
 
-const app = express();
-app.use(express.json());
-app.use(cors({
-    origin: "*",
-    credentials: true
-  }));
-  
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+    const app = express();
+    app.use(express.json());
+    app.use(cors({
+        origin: "*",
+        credentials: true
+    }));
 
-// Static files (for serving uploaded images)
-app.use('/uploads', express.static(path.join(__dirname, 'uploads/')));
-
-
-// Routes
-app.use('/api/admin', adminRoutes);
-app.use('/api/', FacultyRoutes);
-
+    app.use(cookieParser());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// Database connection
-mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then((con) => {
-    console.log(`MongoDB Connected to host: ` + con.connection.host);
-}).catch(err => {
-    console.error('MongoDB connection error:', err.message);
-});
+    app.use('/uploads', express.static(path.join(__dirname, 'uploads/')));
+    app.use('/api/admin', adminRoutes);
+    app.use('/api/', FacultyRoutes);
 
-// Start server
-app.listen(5000, () => {
-    console.log(`Server running on the port 5000`);
-});
+    app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-module.exports = upload; // Export upload for use in routes
+    // Fallback to index.html for other routes (SPA behavior)
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "public/index.html"));
+    });
+
+    app.listen(5000, () => {
+        console.log(`Server running on the port 5000`);
+    });
+
+    module.exports = upload; // Export upload for use in routes
